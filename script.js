e7d097214efa87309ff058a5971bee495fe8d10b
vxnus1998-arch <vxnus1998@gmail.com>
2026-08-03 00:03:54 +1000

    Add IntersectionObserver to manage artist section visibility

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const artistSection = document.querySelector(".artist-section");
 
+const artistObserver = new IntersectionObserver(
+(entries) => {
+entries.forEach((entry) => {
+if (entry.isIntersecting) {
+artistSection.classList.add("is-visible");
+}
+});
+},
+{
+threshold: 0.3
+}
+);
+
+artistObserver.observe(artistSection);
